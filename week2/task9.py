98325a3d8b451e72bd45c1ee1937cdf8ab1030ba
Dias <51242971+Diaskaribaev@users.noreply.github.com>
2022-03-31 11:41:47 +0600

    Create task9.py

---
+++ week2/task9.py
@@ -0,0 +1,23 @@
+n,a,b = int(input()),int(input()),int(input())
+
+h = round((b-a)/n)
+
+print(h)
+
+
+print ("--------------------------")
+
+print (a)
+for x in range (1,n+1):
+    print(a + x * h)
+
+
+
+
+
+
+
+
+
+
+
